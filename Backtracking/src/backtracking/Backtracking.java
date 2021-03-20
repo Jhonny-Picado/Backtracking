67314a8eb0f44955d393fa7a2b31108f9d49b267
Jhonny Picado Vega <jhonnypv14@estudiantec.cr>
2021-03-20 14:17:12 -0600

    Auto stash before merge of "master" and "origin/master"

--- Backtracking/src/backtracking/Backtracking.java
+++ Backtracking/src/backtracking/Backtracking.java
@@ -14,6 +14,24 @@ public class Backtracking {
     /**
      * @param args the command line arguments
      */
+    public static void main(String[] args) {
+       
+        ListaSimple l = new ListaSimple("Sospechosos");
+        CargarCartas(l);
+        l.imprimir();
+        
+    }
     
+    
+    public static void CargarCartas(ListaSimple j){
+        j.insertar(new Nodo("Mejor amigo"));
+        j.insertar(new Nodo("Novio"));
+        j.insertar(new Nodo("Vecino"));
+        j.insertar(new Nodo("Mensajero"));
+        j.insertar(new Nodo("Extraño"));
+        j.insertar(new Nodo("Hermanastro"));
+        j.insertar(new Nodo("Compañero de Trabajo"));
+       
+    }
     
 }
