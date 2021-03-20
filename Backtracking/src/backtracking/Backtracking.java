deac466f1605203eb1d6a142bdccddb177577160
Jocxan <jocxansandi7@gmail.com>
2021-03-20 14:16:28 -0600

    Primera

--- Backtracking/src/backtracking/Backtracking.java
+++ Backtracking/src/backtracking/Backtracking.java
@@ -14,18 +14,6 @@ public class Backtracking {
     /**
      * @param args the command line arguments
      */
-    public static void main(String[] args) {
-       
-        ListaSimple l = new ListaSimple("Sospechosos");
-        
-        l.insertar(new Nodo("Mejor amigo"));
-        l.insertar(new Nodo("Novio"));
-        l.insertar(new Nodo("Vecino"));
-        l.insertar(new Nodo("Mensajero"));
-        l.insertar(new Nodo("Extraño"));
-        l.insertar(new Nodo("Hermanastro"));
-        l.insertar(new Nodo("Compañero de Trabajo"));
-        l.imprimir();
-    }
+    
     
 }
---
+++ Backtracking/src/backtracking/Pruebas.java
@@ -0,0 +1,70 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package backtracking;
+
+
+public class Pruebas {
+    public static void main(String[] args) {
+       ListaSimple Sospechoso = new ListaSimple("Sospechosos");
+       ListaSimple Arma = new ListaSimple("Armas");
+       ListaSimple Motivo = new ListaSimple("Motivos");
+       ListaSimple Parte = new ListaSimple("Partes");
+       ListaSimple Lugar = new ListaSimple("Lugares");
+       
+       CargarCartas(Sospechoso,Arma,Motivo,Parte,Lugar);
+    }
+    
+    public static void CargarCartas(ListaSimple a, ListaSimple b, ListaSimple c, ListaSimple d, ListaSimple e){
+        
+        a.insertar(new Nodo("Mejor amigo"));
+        a.insertar(new Nodo("Novio"));
+        a.insertar(new Nodo("Vecino"));
+        a.insertar(new Nodo("Mensajero"));
+        a.insertar(new Nodo("Extraño"));
+        a.insertar(new Nodo("Hermanastro"));
+        a.insertar(new Nodo("Compañero de Trabajo"));
+        
+        b.insertar(new Nodo("Pistola"));
+        b.insertar(new Nodo("Cuchillo"));
+        b.insertar(new Nodo("Machete"));
+        b.insertar(new Nodo("Pala"));
+        b.insertar(new Nodo("Bate"));
+        b.insertar(new Nodo("Botella"));
+        b.insertar(new Nodo("Tubo"));
+        b.insertar(new Nodo("Cuerda"));
+        
+        
+        c.insertar(new Nodo("Venganza"));
+        c.insertar(new Nodo("Celos"));
+        c.insertar(new Nodo("Dinero"));
+        c.insertar(new Nodo("Accidente"));
+        c.insertar(new Nodo("Drogas"));
+        c.insertar(new Nodo("Robo"));
+        
+        d.insertar(new Nodo("Cabeza"));
+        d.insertar(new Nodo("Pecho"));
+        d.insertar(new Nodo("Abdomen"));
+        d.insertar(new Nodo("Espalda"));
+        d.insertar(new Nodo("Piernas"));
+        d.insertar(new Nodo("Brazos"));
+        
+        e.insertar(new Nodo("Sala"));
+        e.insertar(new Nodo("Comedor"));
+        e.insertar(new Nodo("Baño"));
+        e.insertar(new Nodo("Terraza"));
+        e.insertar(new Nodo("Cuarto"));
+        e.insertar(new Nodo("Garage"));
+        e.insertar(new Nodo("Patio"));
+        e.insertar(new Nodo("Balcón"));
+        e.insertar(new Nodo("Cocina"));
+        
+        
+       
+        
+        
+       
+    }
+}
